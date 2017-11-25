4c6983dab56d428d2a2cd87ce6f4f1bef553763f
Thomas Rory Gummerson <thomas@gummerson.no>
2017-11-25 20:39:56 +0100

    Domain change

--- devs.sh/index.js
+++ devs.sh/index.js
@@ -3,11 +3,11 @@
 const axios = require('axios');
 
 const shortenArticleLink = article =>
-	axios.post('https://devs.rest/',
+	axios.post('https://devs.sh/',
 		article.input_message_content.message_text).then(res =>
 		Object.assign({}, article, {
 			input_message_content: Object.assign({}, article.input_message_content, {
-				message_text: 'https://devs.rest/' + res.data
+				message_text: 'https://devs.sh/' + res.data
 			})
 		}));
 
